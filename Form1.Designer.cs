02451ffda4820cf7eac4f4aa61b07f307106456b
Creedon <leonardzhou@live.com>
2020-12-09 06:01:33 +0800

    update1.1 add save/load info

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,14 +30,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.richTextBox = new System.Windows.Forms.RichTextBox();
-            this.buttonSave = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.buttonRead = new System.Windows.Forms.Button();
             this.buttonTitle1 = new System.Windows.Forms.Button();
             this.buttonTitle2 = new System.Windows.Forms.Button();
             this.buttonTitle3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.buttonSaveFile = new System.Windows.Forms.Button();
+            this.buttonReadFile = new System.Windows.Forms.Button();
             this.buttonContent = new System.Windows.Forms.Button();
             this.buttonContentItalic = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
@@ -54,32 +52,11 @@
             this.richTextBox.Size = new System.Drawing.Size(580, 402);
             this.richTextBox.TabIndex = 0;
             this.richTextBox.Text = "";
-            // 
-            // buttonSave
-            // 
-            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonSave.Location = new System.Drawing.Point(682, 57);
-            this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(86, 23);
-            this.buttonSave.TabIndex = 1;
-            this.buttonSave.Text = "保存";
-            this.buttonSave.UseVisualStyleBackColor = true;
-            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            this.richTextBox.TextChanged += new System.EventHandler(this.richTextBox_TextChanged);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // buttonRead
-            // 
-            this.buttonRead.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonRead.Location = new System.Drawing.Point(682, 14);
-            this.buttonRead.Name = "buttonRead";
-            this.buttonRead.Size = new System.Drawing.Size(86, 23);
-            this.buttonRead.TabIndex = 2;
-            this.buttonRead.Text = "读取";
-            this.buttonRead.UseVisualStyleBackColor = true;
-            this.buttonRead.Click += new System.EventHandler(this.buttonRead_Click);
             // 
             // buttonTitle1
             // 
@@ -114,27 +91,27 @@
             this.buttonTitle3.UseVisualStyleBackColor = true;
             this.buttonTitle3.Click += new System.EventHandler(this.buttonTitle3_Click);
             // 
-            // button2
+            // buttonSaveFile
             // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(643, 58);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 41);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "保存";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.buttonSave_Click);
+            this.buttonSaveFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSaveFile.Location = new System.Drawing.Point(643, 62);
+            this.buttonSaveFile.Name = "buttonSaveFile";
+            this.buttonSaveFile.Size = new System.Drawing.Size(125, 41);
+            this.buttonSaveFile.TabIndex = 1;
+            this.buttonSaveFile.Text = "保存";
+            this.buttonSaveFile.UseVisualStyleBackColor = true;
+            this.buttonSaveFile.Click += new System.EventHandler(this.buttonSave_Click);
             // 
-            // button3
+            // buttonReadFile
             // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Location = new System.Drawing.Point(643, 15);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(125, 41);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "读取";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.buttonRead_Click);
+            this.buttonReadFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonReadFile.Location = new System.Drawing.Point(643, 15);
+            this.buttonReadFile.Name = "buttonReadFile";
+            this.buttonReadFile.Size = new System.Drawing.Size(125, 41);
+            this.buttonReadFile.TabIndex = 2;
+            this.buttonReadFile.Text = "读取";
+            this.buttonReadFile.UseVisualStyleBackColor = true;
+            this.buttonReadFile.Click += new System.EventHandler(this.buttonRead_Click);
             // 
             // buttonContent
             // 
@@ -173,29 +150,29 @@
             this.Controls.Add(this.buttonTitle3);
             this.Controls.Add(this.buttonTitle2);
             this.Controls.Add(this.buttonTitle1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.buttonRead);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonReadFile);
+            this.Controls.Add(this.buttonSaveFile);
             this.Controls.Add(this.richTextBox);
-            this.Controls.Add(this.buttonSave);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "REW RTF Reader";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Button buttonSave;
         private System.Windows.Forms.RichTextBox richTextBox;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button buttonRead;
         private System.Windows.Forms.Button buttonTitle1;
         private System.Windows.Forms.Button buttonTitle2;
         private System.Windows.Forms.Button buttonTitle3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button buttonSaveFile;
+        private System.Windows.Forms.Button buttonReadFile;
         private System.Windows.Forms.Button buttonContent;
         private System.Windows.Forms.Button buttonContentItalic;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,10 @@ namespace RichTextReader
 {
     public partial class Form1 : Form
     {
+        private const string formName = "REW Text Reader"; 
+        private string currentFileName = null;
+        private bool isChanged = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -28,16 +32,41 @@ namespace RichTextReader
         /// <param name="e"></param>
         private void Form1_Load(object sender, EventArgs e)
         {
+            //重设名称
+            ResetFormName();
+
+            //打开命令
             string cmd = Environment.CommandLine;
-
             string[] para = cmd.Split('\"');
-
             if(para.Length > 3)
             {
                 string path = para[3];
                 string rtf = System.IO.File.ReadAllText(path);
                 richTextBox.Rtf = rtf;
+
+                currentFileName = path;
+                isChanged = false;
+                ResetFormName();
             }
+
+            //全屏
+            this.WindowState = FormWindowState.Maximized;
+        }
+        /// <summary>
+        /// 重新显示名字
+        /// </summary>
+        /// <param name="str"></param>
+        private void ResetFormName()
+        {
+            if(currentFileName == null)
+            {
+                this.Text = formName;
+            }
+            else
+            {
+                this.Text = formName + "(" + currentFileName.Substring(currentFileName.LastIndexOf('\\') + 1) + ")" + (isChanged ? "*" : "");
+            }
+            
         }
         /// <summary>
         /// 保存
@@ -46,17 +75,29 @@ namespace RichTextReader
         /// <param name="e"></param>
         private void buttonSave_Click(object sender, EventArgs e)
         {
-            saveFileDialog1.Filter = "RichText(*.rewrtf)|*.rewrtf";
-            DialogResult result = saveFileDialog1.ShowDialog();
-            
-            if(result == DialogResult.OK)
+            if (System.IO.File.Exists(currentFileName))
             {
-                string fileName = saveFileDialog1.FileName;
-                saveFileDialog1.FileOk += ((x, y) => { MessageBox.Show(fileName); });
-                System.IO.File.WriteAllText(fileName, richTextBox.Rtf);
-            }
-            //System.IO.File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test.rewrtf", richTextBox.Rtf);
+                System.IO.File.WriteAllText(currentFileName, richTextBox.Rtf);
+                isChanged = false;
+                ResetFormName();
 
+                MessageBox.Show("保存成功");
+            }
+            else
+            {
+                MessageBox.Show("目录不存在");
+            }
+            #region 另存为代码
+            //saveFileDialog1.Filter = "RichText(*.rewrtf)|*.rewrtf";
+            //DialogResult result = saveFileDialog1.ShowDialog();
+
+            //if (result == DialogResult.OK)
+            //{
+            //    string fileName = saveFileDialog1.FileName;
+            //    saveFileDialog1.FileOk += ((x, y) => { MessageBox.Show(fileName); });
+            //    System.IO.File.WriteAllText(fileName, richTextBox.Rtf);
+            //}
+            #endregion
         }
         /// <summary>
         /// 读取
@@ -71,6 +112,10 @@ namespace RichTextReader
                 string fileName = openFileDialog1.FileName;
                 string rtf = System.IO.File.ReadAllText(fileName);
                 richTextBox.Rtf = rtf;
+
+                currentFileName = fileName;
+                isChanged = false;
+                ResetFormName();
             }
         }
         /// <summary>
@@ -80,23 +125,26 @@ namespace RichTextReader
         /// <param name="e"></param>
         private void buttonTitle1_Click(object sender, EventArgs e)
         {
-            richTextBox.SelectionFont = new Font("黑体", 36f, (FontStyle.Bold));
+            richTextBox.SelectionFont = new Font("黑体", 32f, (FontStyle.Bold));
             richTextBox.SelectionAlignment = HorizontalAlignment.Center;
             richTextBox.SelectionIndent = 0;
+            richTextBox.SelectionHangingIndent = 0;
         }
 
         private void buttonTitle2_Click(object sender, EventArgs e)
         {
-            richTextBox.SelectionFont = new Font("黑体", 24f, (FontStyle.Underline | FontStyle.Bold));
+            richTextBox.SelectionFont = new Font("黑体", 20f, (FontStyle.Underline | FontStyle.Bold));
             richTextBox.SelectionAlignment = HorizontalAlignment.Left;
             richTextBox.SelectionIndent = 0;
+            richTextBox.SelectionHangingIndent = 0;
         }
 
         private void buttonTitle3_Click(object sender, EventArgs e)
         {
-            richTextBox.SelectionFont = new Font("宋体", 18f, (FontStyle.Bold));
+            richTextBox.SelectionFont = new Font("宋体", 16f, (FontStyle.Bold));
             richTextBox.SelectionAlignment = HorizontalAlignment.Left;
             richTextBox.SelectionIndent = 0;
+            richTextBox.SelectionHangingIndent = 0;
         }
 
         private void buttonContent_Click(object sender, EventArgs e)
@@ -104,6 +152,7 @@ namespace RichTextReader
             richTextBox.SelectionFont = new Font("宋体", 12f, (FontStyle.Regular));
             richTextBox.SelectionAlignment = HorizontalAlignment.Left;
             richTextBox.SelectionIndent = 20;
+            richTextBox.SelectionHangingIndent = -20;
         }
 
         private void buttonContentItalic_Click(object sender, EventArgs e)
@@ -111,6 +160,40 @@ namespace RichTextReader
             richTextBox.SelectionFont = new Font("宋体", 12f, (FontStyle.Italic));
             richTextBox.SelectionAlignment = HorizontalAlignment.Left;
             richTextBox.SelectionIndent = 20;
+            richTextBox.SelectionHangingIndent = -20;
+        }
+        /// <summary>
+        /// 文字改变
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void richTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!isChanged)
+            {
+                isChanged = true;
+                ResetFormName();
+            }
+        }
+        /// <summary>
+        /// 事件：KeyDown
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.S && e.Modifiers == Keys.Control)
+            {
+                buttonSaveFile.PerformClick();
+            }
+        }
+        /// <summary>
+        /// 事件：KeyPress
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
         }
     }
 }
